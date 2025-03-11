27d0168fbe65ab54be4975ca502ac602f7cf1248
Farooq Ahmed <03174748082ali@gmail.com>
2025-03-11 15:49:22 +0500

    started comerision operator

---
+++ app.js
@@ -0,0 +1,33 @@
+//Comparision Operator
+ 
+ // >
+ // <
+ // ==
+ // === 
+ // != 
+ // !== 
+ // >=
+ // <=
+ 
+ // console.log(50 > 20);
+ // console.log(50 < 20);
+ // console.log(50 == '50');
+ // console.log(50 === 50);
+ 
+ // console.log(50 !== '50');
+ 
+ 
+ // console.log(20 >= 30);
+ // console.log(20 <= 20);
+ 
+ 
+ 
+ 
+ // == data type check nahi krta
+ // === data type check krta ha
+ // != data type check nahi krta ==
+ // !== data type check  krta ha ===
+ 
+ 
+ // console.log(40 == '40');
+ 
